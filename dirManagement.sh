39a9a5ea1e70d36cf0ac196d5bfbc0c5c4417114
cat-friend <denisepli@gmail.com>
2022-12-05 21:30:06 -0800

    made script for starter repos

--- dirManagement.sh
+++ dirManagement.sh
@@ -3,8 +3,7 @@
 for i in {05..25};
 do
     mkdir 12-${i}
-    mv 12-${i}.js 12-${i}
-    touch 12-${i}/input.js
+    touch 12-${i}/input.js 12-${i}/12-${i}.js
     printf 'export const input = ``;' > 12-${i}/input.js
     printf "import { input } from './input.js';" > 12-${i}/12-${i}.js
 done
