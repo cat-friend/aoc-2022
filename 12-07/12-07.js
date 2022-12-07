a0f00f70cdd8718bba9e8a6d5b23004301e2a8d6
cat-friend <denisepli@gmail.com>
2022-12-06 21:57:13 -0800

    spinning my wheels, 12-07 not done

--- 12-07/12-07.js
+++ 12-07/12-07.js
@@ -1,1 +1,91 @@
-import { input } from './input.js';
+import { input, test } from './input.js';
+
+function dirSizeFinder(input, part1 = true) {
+
+}
+
+function dirGraph(input) {
+
+}
+
+class Directory {
+    constructor() {
+        this.root = undefined;
+    }
+
+    get over1000() {
+
+    }
+}
+
+class dirNode {
+    constructor({ dirName, size }) {
+        this.dirName = dirName;
+        this.size = size;
+        this.children;
+        this.parent;
+    }
+}
+/**
+ * {dirE: {
+ * parent:
+ * }}
+ */
+function dirMapMaker(info) {
+    const directory = [];
+    const terminalOutput = info.split("\n");
+    let size = 0;
+    let dirName;
+    let currNode;
+    let children;
+    let i = 0;
+    for (const line of terminalOutput) {
+        const lineInfo = lineParser(line);
+        switch (lineInfo.type) {
+            case 'dirName':
+                size = 0;
+                dirName = { lineInfo };
+                currNode = new dirNode({ dirName, size });
+                directory.push(currNode);
+                break;
+            case 'file':
+                currNode.size += line.size;
+                break;
+            default:
+                break;
+        }
+    }
+
+}
+
+function lineParser(line) {
+    const nums = "0123456789";
+    if (line.startsWith("$ l")) {
+        return {
+            type: "ls"
+        }
+    }
+    if (line === "$ cd ..") {
+        const dirName = line.split(" ").pop();
+        return {
+            type: "moveUp",
+            dirName,
+        }
+    }
+    if (nums.includes(line[0])) {
+        const [size, fileName] = line.split(" ");
+        return {
+            type: "file",
+            size,
+            fileName
+        }
+    }
+    if (line.startsWith("dir")) {
+        const [cmd, dirName] = line.split(" ");
+        return {
+            type: "child",
+            dirName,
+        }
+    }
+    return { type: "noAction" }
+}
